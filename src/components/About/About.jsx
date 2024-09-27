4f3ea46a2f44925fac31b8413710ee80e6e080e6
pauzuffi <paula.zuffinetti@gmail.com>
2024-09-27 12:04:46 -0300

    detalles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,8 +12,9 @@ const About = () => {
 
       <div className="about-right">
         <h3>Sobre mi</h3>
-        <p>Mi nombre es Pau, soy Técnica Cosmetóloga recibida de la UBA. Realizo tanto tratamientos faciales como corporales, tratando la piel no como algo estético sino como parte del cuidado de la salud. La piel es un órgano y hay que cuidarlo como tal.</p>
-        <p>Además de realizar un tratamiento, te voy a asesorar y a enseñarte cómo cuidar tu piel, armando una rutina 100% personalizada en base a tu tipo de piel, necesidades y factores externos.</p>
+        <p>Mi nombre es Pau, soy Técnica Cosmetóloga recibida de la UBA. Me especializo en tratamientos faciales y corporales, enfocándome en la piel no solo desde un aspecto estético, sino como un componente esencial del cuidado de la salud. La piel es el órgano más grande de nuestro cuerpo, y merece la misma atención y cuidado.</p>
+        <p>Además de realizar un tratamiento adecuado para vos, te voy a asesorar y a enseñar cómo cuidar tu piel, armando una rutina 100% personalizada en base a tu tipo de piel, necesidades y otros factores que puedan influir en tu bienestar.</p>
+        <p>¿Es la primera vez que te haces algún tratamiento?¿No sabes por dónde empezar? No te preocupes,¡estoy para ayudarte!</p>
 
       </div>
     </div>
